56b1f3bf3d9115bc58396bf694c8a1b0c3bd2ee4
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-11-29 21:53:02 -0500

    withDraw method tested

--- BankAccountTester.java
+++ BankAccountTester.java
@@ -10,5 +10,12 @@ public class BankAccountTester {
     System.out.println(ba.getBalance());
     System.out.println(ba.deposit(-12.34));
     System.out.println(ba.getBalance());
+
+    System.out.println();
+
+    System.out.println(ba.withDraw(10.00));
+    System.out.println(ba.getBalance());
+    System.out.println(ba.withDraw(1000.00));
+    System.out.println(ba.getBalance());
   }
 }
